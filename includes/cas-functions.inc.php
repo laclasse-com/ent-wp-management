300e31239016c0a6a958d4650df5c42ac0304ae6
Daniel LACROIX <dlacroix@erasme.org>
2017-09-19 09:01:11 +0200

    remove phpCAS

--- includes/cas-functions.inc.php
+++ includes/cas-functions.inc.php
@@ -1,66 +1,20 @@
 <?php
-/* 
- Copyright (C) 2008 Casey Bisson
-
- This plugin owes a huge debt to 
- Stephen Schwink's CAS Authentication plugin, copyright (C) 2008 
- and released under GPL. 
- http://wordpress.org/extend/plugins/cas-authentication/
-
- This plugin honors and extends Schwink's work, and is licensed under the same terms.
-
- This Plugin was hardy modified by Pierre-Gilles Levallois for www.laclasse.com.
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	 02111-1307	 USA 
-*/
 
 // --------------------------------------------------------------------------------
 //  Fonction de provisionning CAS
 // --------------------------------------------------------------------------------
-function wpcas_provisioning(){
+function wpcas_provisioning($userENT, $user_email){
 	global $domain;
-	$user_id = phpCAS::getAttribute('uid');
-	$user_login = phpCAS::getAttribute('login');
-	$user_email = phpCAS::getAttribute('uid') . "@noemail.lan";
-	if (phpCAS::hasAttribute('MailAdressePrincipal') && (phpCAS::getAttribute('MailAdressePrincipal') != ""))
-		$user_email = phpCAS::getAttribute('MailAdressePrincipal');
-	else if (phpCAS::hasAttribute('LaclasseEmail') && (phpCAS::getAttribute('LaclasseEmail') != ""))
-		$user_email = phpCAS::getAttribute('LaclasseEmail');
+	$user_id = $userENT->id;
+	$user_login = $userENT->login;
 
 	$blogId = get_blog_id_by_domain($domain);
 	$blogData = getBlogData($blogId);
 
-	$userENT = json_decode(get_http(ANNUAIRE_URL."api/users/$user_id?expand=true"));
     $role = getUserWpRole($userENT, $blogData);
 
 	createUserWP($user_login, $user_email);
 	redirection($domain);
-}
-
-// --------------------------------------------------------------------------------
-//  Formulaire de choix d'un serveur de sso
-// --------------------------------------------------------------------------------
-function select_sso() {
-  global $wpcas_options;
-  $html = "\n<form action='".home_url()."/wp-login.php'>\n<select name='ent' id='ent' class=''>
-	<option value=''>Choisir un serveur d'authentification</option>";
-	foreach($wpcas_options as $k => $v) {
-	 $html .= "<option value='$k'>$k</option>\n";
-	}
-  $html .= "</select><br/><br/><input type='submit' value='Valider'/></form>";
-  return $html;
 }
 
 // --------------------------------------------------------------------------------
@@ -73,113 +27,92 @@ class wpCAS {
 	 If the user is not provisioned, wpcas_nowpuser() is called
 	*/
 	public static function authenticate() {
-		global $wpcas_options, $cas_configured, $ent, $_REQUEST;
+		global $wpcas_options, $ent, $_REQUEST;
 
-		error_log("DANIEL authenticate");
-		error_log(print_r($_REQUEST, true));
-		error_log("TICKET: " . $_REQUEST['ticket']);
-
-		logIt("<h1>ENT : $ent</h1>");
-		$proto = ($wpcas_options[$ent]['server_port'] == '443') ? 's': '';
-		logIt("Serveur d'authentification : http".$proto."://".$wpcas_options[$ent]['server_hostname'].":".$wpcas_options[$ent]['server_port'].$wpcas_options[$ent]['server_path'].".");
-
-		if ( !$cas_configured ) {
-			message('<h1>Aucun serveur d\'authentification trouv&eacute;'.((!isset($ent)||$ent =='')? '' : ' pour l\'ENT "'.$ent.'"').'.</h1>'.
-			        '<br/>S&eacute;lectionnez votre ENT : <br/><br/>' . 
-			        select_sso()
-			       );
-			die();
-		}
-
-/*		if (isset($_REQUEST['ticket'])) {
+		if (isset($_REQUEST['ticket'])) {
 			$req = curl_init();
-	        curl_setopt($req, CURLOPT_URL, 'https://v3dev.laclasse.lan/sso/serviceValidate?ticket=' .
-				urlencode($_REQUEST['ticket']) . '&service=' . get_url_login());
-			curl_exec($req);
+	        curl_setopt($req, CURLOPT_URL, wpCAS::get_url_sso() . 'serviceValidate?ticket=' .
+				urlencode($_REQUEST['ticket']) . '&service=' . wpCAS::get_url_login());
+			curl_setopt($req, CURLOPT_RETURNTRANSFER, 1);
+			$data = curl_exec($req);
 			$error = curl_errno($req);
-		}*/
+			$http_status = curl_getinfo($req, CURLINFO_HTTP_CODE);
+			curl_close($req);
 
-		//if(is_user_logged_in()) {
-		//}
+			if ($http_status == 200) {
+				// parse the ticket content
+				$xml = simplexml_load_string($data);
+				// get the uid attribute
+				$xml->registerXPathNamespace('cas', 'http://www.yale.edu/tp/cas');
+				$uids = $xml->xpath("//cas:uid");
+				if (count($uids) > 0) {
+					$uid = (string)$uids[0];
+					$userENT = json_decode(get_http(ANNUAIRE_URL."api/users/$uid?expand=true"));
 
-		if( phpCAS::isAuthenticated() ){
-			// CAS was successful so sets session variables
-			$user = get_user_by('login', phpCAS::getUser());
-			if ( $user ){ // user already exists
-				// the CAS user has a WP account
-				wp_set_auth_cookie( $user->ID );
-				wp_set_current_user( $user->ID );
-				
-				// Enregistrement de l'ENT de provenance pour ce username
-	   			if(phpCAS::getAttribute('LaclassePrenom') && phpCAS::getAttribute('LaclasseNom')) {
+					$user_email;
+					foreach($userENT->emails as $email) {
+						if (!isset($user_email) || $email->primary)
+							$user_email = $email->address;
+					}
+					if (!isset($user_email))
+						$user_email = $user->id . '@noemail.lan';
 
-					$user_email = phpCAS::getAttribute('uid') . "@noemail.lan";
-					if (phpCAS::hasAttribute('MailAdressePrincipal') && (phpCAS::getAttribute('MailAdressePrincipal') != ""))
-						$user_email = phpCAS::getAttribute('MailAdressePrincipal');
-					else if (phpCAS::hasAttribute('LaclasseEmail') && (phpCAS::getAttribute('LaclasseEmail') != ""))
-						$user_email = phpCAS::getAttribute('LaclasseEmail');
+					$user = get_user_by('login', $userENT->login);
+					// if user already exists
+					if ($user) {
+						// the CAS user has a WP account
+						wp_set_auth_cookie($user->ID);
+						wp_set_current_user($user->ID);
 
-	                // Met ï¿½ jour les donnï¿½es de l'utilisateur
-					wp_update_user( 
-						array (
-							'ID' => $user->ID, 
-							'first_name' => phpCAS::getAttribute('LaclassePrenom'), 
-							'last_name' => phpCAS::getAttribute('LaclasseNom'),
-							'display_name' => phpCAS::getAttribute('LaclasseNom').' '.
-								phpCAS::getAttribute('LaclassePrenom'),
-							'user_email' => $user_email
-						)
-					);
+		                // Met à jour les données de l'utilisateur
+						wp_update_user( 
+							array (
+								'ID' => $user->ID, 
+								'first_name' => $userENT->firstname, 
+								'last_name' => $userENT->lastname,
+								'display_name' => $userENT->lastname . ' '.
+									$userENT->firstname,
+								'user_email' => $user_email
+							)
+						);
+					}
+					// On provisionne ce qu'il manque : user ou blog, ou les deux
+					wpcas_provisioning($userENT, $user_email);
 				}
 			}
-			// On provisionne ce qu'il manque : user ou blog, ou les deux
-			wpcas_provisioning();
-		
 		}
 		else {
-			// hey, authenticate
-			phpCAS::forceAuthentication();
+			header('Location: ' . wpCAS::get_url_login());
 			die();
 		}
 	}
-	
-	// renvoie l'url de login, selon le contexte : intï¿½grï¿½ dans une IFRAME ou normal
-	public static function get_url_login() {
-		global $ent, $wpcas_options, $cas_configured;
 
-		error_log("DANIEL get_url_login");
+	public static function get_url_sso() {
+		global $ent, $wpcas_options;
 
-		// Si CAS n'est pas configuré, on retourne une url de login standard.
-		if (!$cas_configured){
-		  return home_url()."/wp-login.php?ent=$ent";
-		}
-		
-		$protoc = ($wpcas_options[$ent]['server_port'] == 443) ? "https://" : "http://";
-		$url = $protoc.
+		return 
+			($wpcas_options[$ent]['server_port'] == 443) ? "https://" : "http://".
 			$wpcas_options[$ent]['server_hostname'].
 			(($wpcas_options[$ent]['server_port'] != 80 )? ":".$wpcas_options[$ent]['server_port'] : "").
-			$wpcas_options[$ent]['server_path'].
-			"/login?service=".urlencode(home_url()."/wp-login.php?ent=".$ent);
-			   
-		return $url;
+			$wpcas_options[$ent]['server_path'] . "/";
+	}
+
+	// renvoie l'url de login, selon le contexte : intï¿½grï¿½ dans une IFRAME ou normal
+	public static function get_url_login() {
+		global $ent;
+		return wpCAS::get_url_sso() . "login?service=" . urlencode(home_url() . "/wp-login.php?ent=".$ent);
 	}
 	
 	// Revoie l'url de logout selon l'ent de provenance.
 	public static function get_url_logout($wpLogoutUrl) {
-		error_log("DANIEL get_url_logout");
-
-		$current_user = wp_get_current_user();
-		$nomEnt = get_user_meta( $current_user->ID, "nom_ENT", true);
-		return $wpLogoutUrl . "&ent=".$nomEnt;
+		return $wpLogoutUrl . "&ent=laclasse";
 	}
 	
 	// hook CAS logout to WP logout
 	public static function logout() {
-	    // Supprimer les cookies de WP
-	    wp_destroy_current_session();
-	    wp_clear_auth_cookie();
-		phpCAS::logout( array( 'url' => get_option( 'siteurl' )."?ent=laclasse".$_REQUEST['ent']));
-		exit();
+		global $ent;
+		header('Location: ' . wpCAS::get_url_sso() . "logout?service=" . urlencode(home_url() . "/wp-login.php?ent=".$ent));
+		die();
 	}
 
 	// hide password fields on user profile page.
@@ -205,38 +138,5 @@ class wpCAS {
 	}
 }
 
-// --------------------------------------------------------------------------------
-//  D ï¿½ b u t   d u   s c r i p t   d e   C A S i f i c a t i o n 
-// --------------------------------------------------------------------------------
 $ent = "laclasse";
-if (isset($_REQUEST['ent']) && ($_REQUEST['ent'] != "")) $ent = $_REQUEST['ent'];
-
-$cas_configured = true;
-
-// try to configure the phpCAS client
-if ($wpcas_options[$ent]['include_path'] == '' ||
-		(include_once $wpcas_options[$ent]['include_path']) != true) {
-	$cas_configured = false;
-}
-
-if ($wpcas_options[$ent]['server_hostname'] == '' ||
-		intval($wpcas_options[$ent]['server_port']) == 0) {
-	$cas_configured = false;
-}
-
-if ($cas_configured) {
-	phpCAS::client($wpcas_options[$ent]['cas_version'], 
-		$wpcas_options[$ent]['server_hostname'], 
-		intval($wpcas_options[$ent]['server_port']), 
-		$wpcas_options[$ent]['server_path']);
-	
-	// function added in phpCAS v. 0.6.0
-	// checking for static method existance is frustrating in php4
-	$phpCas = new phpCas();
-	if (method_exists($phpCas, 'setNoCasServerValidation'))
-		phpCAS::setNoCasServerValidation();
-	unset($phpCas);
-	
-	// if you want to set a cert, replace the above few lines
-}
 
