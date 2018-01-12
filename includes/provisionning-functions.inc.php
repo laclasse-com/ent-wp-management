f94a2171e05c393bcc979fc9f1ff69f3cd20f5b6
Nelson Gonçalves <ngoncalves@erasme.org>
2018-01-12 11:13:08 +0100

    changes wordpress theme and widgets when creating a blog

--- includes/provisionning-functions.inc.php
+++ includes/provisionning-functions.inc.php
@@ -40,5 +40,40 @@ function creerNouveauBlog($domain, $path, $sitename, $username, $user_email, $si
 
 	update_blog_option($wpBlogId, 'comment_registration', 1 );
 	
+	// Change theme of created blog
+	// Add new widget for Posts (Pages)
+	$laclasseTheme = wp_get_theme('wordpress-theme-laclasse');
+	if($laclasseTheme->exists()) {
+		update_blog_option($wpBlogId,'stylesheet','wordpress-theme-laclasse'); 	
+		insert_widget_in_blog_sidebar('pages',array('sortby' => 'menu_order'), $wpBlogId,'sidebar-1');
+	}
+	
 	return $wpBlogId;
+}
+
+/**
+ * Insert a widget a sidebar, widget always inserted at index 1
+ * 
+ * @param string $widget_id   ID of the widget (search, recent-posts, etc.)
+ * @param array $widget_data  Widget settings.
+ * @param int $wp_blog_id	  ID of the blog in which to add the widget
+ * @param string $sidebar     ID of the sidebar.
+ */
+function insert_widget_in_blog_sidebar( $widget_id, $widget_data, $wp_blog_id ,$sidebar ) {
+	// Retrieve sidebars, widgets and their instances
+	$sidebars_widgets = get_blog_option($wp_blog_id, 'sidebars_widgets', array() );
+	$widget_instances = get_blog_option($wp_blog_id, 'widget_' . $widget_id, array() );
+	// Retrieve the key of the next widget instance
+	$numeric_keys = array_filter( array_keys( $widget_instances ), 'is_int' );
+	$next_key = $numeric_keys ? max( $numeric_keys ) + 1 : 2;
+	// Add this widget to the sidebar
+	if ( ! isset( $sidebars_widgets[ $sidebar ] ) ) {
+		$sidebars_widgets[ $sidebar ] = array();
+	}
+	array_splice($sidebars_widgets[ $sidebar ], 1, 0, array($widget_id . '-' . $next_key));
+	// Add the new widget instance
+	$widget_instances[ $next_key ] = $widget_data;
+	// Store updated sidebars, widgets and their instances
+	update_blog_option($wp_blog_id, 'sidebars_widgets', $sidebars_widgets );
+	update_blog_option($wp_blog_id, 'widget_' . $widget_id, $widget_instances );
 }
