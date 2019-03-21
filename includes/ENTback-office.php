2afd4f4a4be2cf3d49a0e48ae45c8ec625dc6690
Nelson Gonçalves <ngoncalves@erasme.org>
2019-03-21 18:23:23 +0100

    Remove and Restrict delete site from wp-admin

--- includes/ENTback-office.php
+++ includes/ENTback-office.php
@@ -8,8 +8,8 @@
 require_once(ABSPATH  . '/wp-admin/includes/template.php');
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	
-	G E S T I O N   D E   L ' A N O N Y M A T 
+
+	G E S T I O N   D E   L ' A N O N Y M A T
 
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
  // ------------------------------------------------------------------
@@ -21,42 +21,42 @@ require_once(ABSPATH  . '/wp-admin/includes/template.php');
 		'Anonymat des élèves',
 		'explicationAnonymat',
 		'reading');
- 	
+
  	// Ajout du champs "student-privacy" dans cette section
  	add_settings_field('student-privacy',
 		'Signature des élèves',
 		'formulaireAnonymat',
 		'reading',
 		'student_privacy_section');
- 	
+
  	// Enregistrement dans $_POST
  	register_setting('reading','student-privacy');
  }
-   
+
  // ------------------------------------------------------------------
  // Paragraphe d'explication
  // ------------------------------------------------------------------
  function explicationAnonymat() {
- 	echo '<p>Decidez ici de ce que votre blog doit afficher pour la signature publique des 
+ 	echo '<p>Decidez ici de ce que votre blog doit afficher pour la signature publique des
  		élèves dans les articles et les commentaires<br/>
- 		Vous pouvez choisir d\'afficher leurs noms et prénoms, ou bien une chaîne de 
+ 		Vous pouvez choisir d\'afficher leurs noms et prénoms, ou bien une chaîne de
  		caractères qui les représentera tout en leur permettant de rester anonyme.</p>';
  }
- 
+
  // ------------------------------------------------------------------
  // Formulaire
  // ------------------------------------------------------------------
  function formulaireAnonymat() {
- 	echo '<input name="student-privacy" id="student-privacy" type="radio" value="1" class="code" ' . 
- 		checked( 1, get_option('student-privacy'), false ) . 
+ 	echo '<input name="student-privacy" id="student-privacy" type="radio" value="1" class="code" ' .
+ 		checked( 1, get_option('student-privacy'), false ) .
  		' /> Les noms et prénoms sont anonymisés.<br/>';
- 	echo '<input name="student-privacy" id="student-privacy" type="radio" value="0" class="code" ' . 
- 		checked( 0, get_option('student-privacy'), false ) . 
+ 	echo '<input name="student-privacy" id="student-privacy" type="radio" value="0" class="code" ' .
+ 		checked( 0, get_option('student-privacy'), false ) .
  		' /> Les noms et prénoms des élèves apparaîssent dans les articles et les commentaires.';
  }
 
  // ------------------------------------------------------------------
- // Fonction qui renvoie une signature anonyme ou pas en fonctions 
+ // Fonction qui renvoie une signature anonyme ou pas en fonctions
  //	des options du blog
  // ------------------------------------------------------------------
   function getSignature($signature, $userId, $profil){
@@ -69,7 +69,7 @@ require_once(ABSPATH  . '/wp-admin/includes/template.php');
 	  $BackOffice = 0;
 	else
 	  $BackOffice = 1;
-  	
+
   	// si l'anonymat a été paramètré on affiche un nom anonymisé
   	if ($profil == 'ELV' && $choixAnonymat == 1 ) {
   		$uidAnonyme = get_user_meta( $userId, 'uid_ENT');
@@ -98,13 +98,34 @@ require_once(ABSPATH  . '/wp-admin/includes/template.php');
   }
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	
-	h o o k   G E S T I O N   D E   L ' A N O N Y M A T 
+
+	h o o k   G E S T I O N   D E   L ' A N O N Y M A T
 
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
  add_action('admin_init', 'gestionAnonymatEleves', 11);
- 
+
  add_filter('the_author', 'gereAnonymatArticle', 10, 1);
  add_filter('get_comment_author', 'gereAnonymatCommentaire', 10, 3);
 
 
+//
+// Removes unwanted sub-menus
+// Anyone wishing to do so should use our backend so we have better control on what happens
+//
+function admins_remove_menus () {
+	remove_submenu_page( 'tools.php', 'ms-delete-site.php' );	//Delete Site
+}
+add_action( 'admin_menu', 'admins_remove_menus', 999 );
+
+//
+// Actually restrict accessing the sub-menu using the URL
+//
+function restrict_menus() {
+	$screen = get_current_screen();
+	$base   = $screen->id;
+	if( 'ms-delete-site' == $base) {
+		wp_die('You shouldn\'t try to access restricted menus');
+	}
+
+}
+add_action( 'current_screen', 'restrict_menus' );
